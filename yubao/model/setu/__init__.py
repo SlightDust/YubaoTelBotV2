22ad46bc24c88b0224f742bcf181179a886d4379
SlightDust <zzc_200014@163.com>
2023-06-29 02:56:24 +0800

    修改at发送方式

--- yubao/model/setu/__init__.py
+++ yubao/model/setu/__init__.py
@@ -14,6 +14,8 @@ from aiotg.bot import BotApiError
 # 自目录引用
 import yubao
 import yubao.util.aiorequest
+from yubao import extendChat
+from yubao.util.make_at import at_sender, at_callbacker
 from yubao.util.logger import new_logger
 logger = new_logger("setu")
 bot = yubao.get_bot()
@@ -23,7 +25,7 @@ config = Config().read_config()
 logger = new_logger("setu")
 
 @bot.command(r'^[色涩瑟][图圖]|不够[色涩瑟]|[来來发發给給]((?P<num>\d+)|(?:.*))[张張个個幅点點份丶](?P<keyword>.*?)[色涩瑟][图圖]$')
-async def setu(chat:Chat, match):
+async def setu(chat:extendChat, match):
     logger.info(f'收到来自{chat.sender} from {chat.type} chat {chat.id} 的 setu 消息: {chat.message}')
     if 'private' in chat.type:
         chat.reply("请在群聊中使用该功能~")
@@ -46,6 +48,7 @@ async def setu(chat:Chat, match):
         if ("R-18" in tags) and ("private" not in chat.type):
             # R-18图片单独发送
             # chat.send_text("R-18图片已通过私聊发送")
+            attext, at = await at_sender(chat)
             try:
                 r18msg = await bot.send_message(chat_id=chat.message.get('from').get('id'),text=msg)
                 sent_chatid = chat.message.get('from').get('id')
@@ -58,15 +61,15 @@ async def setu(chat:Chat, match):
                         'callback_data': f'showr18pic-{sent_chatid}-{sent_messageid}'}]
                         ]
                     }
-                await chat.send_text(f'@{chat.message.get("from").get("username")} R-18图片已通过私聊发送。\n任何人点击按钮可接收私聊消息。', reply_markup=json.dumps(inlinekeyboardmarkup))
+                await chat.send_text(f'{attext} R-18图片已通过私聊发送。\n任何人点击按钮可接收私聊消息。', reply_markup=json.dumps(inlinekeyboardmarkup), entities=at)
             except BotApiError as e:    # 未认证bot
                 print(e.args)
                 if "bot can't initiate conversation with a user" in str(e.args):
-                    await chat.reply(f'@{chat.message.get("from").get("username")} R-18图片私聊发送失败。请先私聊域宝并发送/start后再试。')
+                    await chat.reply(f'R-18图片私聊发送失败。请先私聊域宝并发送/start后再试。')
                 elif "bot was blocked by the user" in str(e.args): 
-                    await chat.reply(f'@{chat.message.get("from").get("username")} 您屏蔽了域宝的私聊消息。R-18图片发送失败。')
+                    await chat.reply(f'您屏蔽了域宝的私聊消息。R-18图片发送失败。')
                 else:
-                    await chat.reply(f'@{chat.message.get("from").get("username")} R-18图片私聊发送失败: \n{e}')
+                    await chat.reply(f'R-18图片私聊发送失败: \n{e}')
                 pass
         else:
             await chat.send_text(text=msg)
@@ -76,10 +79,20 @@ async def showr18pic(chat, cq, match):
     chat_id = cq.src.get('from').get('id')
     from_chat_id = match.group(1)
     message_id = match.group(2)
+    attext, at = await at_callbacker(cq)
     # print(chat_id, from_chat_id, message_id)
     # print(cq.src)
-    await bot.copy_message(chat_id, from_chat_id, message_id)
-    await bot.send_message(chat_id=cq.src.get('message').get('chat').get('id'),text=f"@{cq.src.get('from').get('username')} 您点击了按钮，私聊请查收~", reply_to_message_id=cq.src.get('message').get('message_id'))
+    try:
+        await bot.copy_message(chat_id, from_chat_id, message_id)
+        await bot.send_message(chat_id=cq.src.get('message').get('chat').get('id'),text=f"{attext} 您点击了按钮，私聊请查收~", reply_to_message_id=cq.src.get('message').get('message_id'), entities=at)
+    except BotApiError as e:    # 未认证bot
+        print(e.args)
+        if "bot can't initiate conversation with a user" in str(e.args):
+            await bot.send_message(chat_id=cq.src.get('message').get('chat').get('id'), text=f'{attext} R-18图片私聊发送失败。请先私聊域宝并发送/start后再试。', entities=at)
+        elif "bot was blocked by the user" in str(e.args): 
+            await bot.send_message(chat_id=cq.src.get('message').get('chat').get('id'), text=f'{attext} 您屏蔽了域宝的私聊消息。R-18图片发送失败。', entities=at)
+        else:
+            await bot.send_message(chat_id=cq.src.get('message').get('chat').get('id'), text=f'{attext} R-18图片私聊发送失败: \n{e}', entities=at)
     
 
 def generate_image_struct():
